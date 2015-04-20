853f204ac2aef91f1cb9170aa075e212a38e802c
zhangchenchen <525513690@qq.com>
2015-04-20 16:21:46 +0800

    可修改滚动时间 添加了是否循环滚动的bool类型

---
+++ ZCScrollewRolling/ZCNormalDefine.h
@@ -0,0 +1,13 @@
+//
+//  ZCNormalDefine.h
+//  ZCScrollewRolling
+//
+//  Created by For_Minho on 15/4/20.
+//  Copyright (c) 2015年 For_Minho. All rights reserved.
+//
+
+#ifndef ZCScrollewRolling_ZCNormalDefine_h
+#define ZCScrollewRolling_ZCNormalDefine_h
+
+
+#endif
